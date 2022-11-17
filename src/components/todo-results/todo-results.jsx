072401b908f6d9425092d8e74b0462b23492f9f9
Harshal <harshalbokade984@gmail.com>
2022-11-17 19:13:58 +0530

    Added an ability to calculate completed tasks

--- src/components/todo-results/todo-results.jsx
+++ src/components/todo-results/todo-results.jsx
@@ -1,15 +1,20 @@
 import * as React from 'react';
+import { TodosContext } from '../../todo-context';
 import './todo-results.scss';
 
 export const TodoResults = () => {
+  const { todos } = React.useContext(TodosContext);
   const calculateChecked = () => {
-    // Fix an ability to calculate completed tasks
+    // Added an ability to calculate completed tasks
+    const completedTodos = todos.filter((todo) => todo.checked === true).length;
+    return completedTodos;
   };
 
-  return (
-    <div className="todo-results">
-      Done:
-      {calculateChecked()}
-    </div>
+return (
+  <div className="todo-results">
+    Done:
+    {' '}
+    {calculateChecked()}
+  </div>
   );
 };
